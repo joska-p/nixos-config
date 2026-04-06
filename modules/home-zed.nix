1a3a3db62766e8ea8e6a3b8b0001bc25c1fd0303
joska <jpotin@gmail.com>
2026-04-06 11:40:37 +0200

    Update home-zed.nix

--- modules/home-zed.nix
+++ modules/home-zed.nix
@@ -2,59 +2,9 @@
 
 {
   programs.zed-editor = {
-
     enable = true;
-
-    # This populates the userSettings "auto_install_extensions"
-    extensions = [
-      "nix"
-      "toml"
-    ];
-
     # Everything inside of these brackets are Zed options
     userSettings = {
-      agent = {
-        default_model = {
-          provider = "copilot_chat";
-          model = "oswe-vscode-prime";
-        };
-        model_parameters = [ ];
-      };
-
-      node = {
-        path = lib.getExe pkgs.nodejs;
-        npm_path = lib.getExe' pkgs.nodejs "npm";
-      };
-
-      auto_update = false;
-
-      terminal = {
-        copy_on_select = false;
-        dock = "bottom";
-        detect_venv = {
-          on = {
-            directories = [
-              ".env"
-              "env"
-              ".venv"
-              "venv"
-            ];
-            activate_script = "default";
-          };
-        };
-        env = {
-          TERM = "alacritty";
-        };
-        font_family = "JetBrainsMono Nerd Font";
-        font_features = null;
-        font_size = null;
-        line_height = "comfortable";
-        option_as_meta = false;
-        button = false;
-        shell = "system";
-        working_directory = "current_project_directory";
-      };
-
       languages = {
         Nix = {
           language_servers = [
@@ -63,13 +13,7 @@
           ];
         };
       };
-
-      vim_mode = false;
-
-      base_keymap = "VSCode";
-
       theme = "Gruvbox Dark";
-
       ui_font_size = 16;
       buffer_font_size = 16;
     };
