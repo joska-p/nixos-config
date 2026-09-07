f22585ca0a56a6bb490912934bf180e3d5ab1306
joska <jpotin@gmail.com>
2026-09-07 16:56:11 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -48,7 +48,7 @@
             { pkgs, lib, vars, ... }:
             {
               imports = [
-               "${vars.configDir}/hardware-configuration.nix"
+               "./hardware-configuration.nix "
               ];
 
               boot.loader.systemd-boot.enable = true;
