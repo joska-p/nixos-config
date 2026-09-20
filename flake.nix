ba34a0362e940d7ef96469da94072f8cf8000b6b
joska <jpotin@gmail.com>
2026-09-20 16:38:52 +0200

    rootless docker

--- flake.nix
+++ flake.nix
@@ -291,7 +291,24 @@
                 };
               };
 
-              virtualisation.docker.enable = true;
+              # ==========================================================
+              # ROOTLESS Docker
+              # ==========================================================
+              virtualisation.docker = {
+                # Consider disabling the system wide Docker daemon
+                enable = false;
+
+                rootless = {
+                  enable = true;
+                  setSocketVariable = true;
+                  # Optionally customize rootless Docker daemon settings
+                  #daemon.settings = {
+                  #  data-root = "~/.local/docker";
+                  #  dns = [ "1.1.1.1" "8.8.8.8" ];
+                  #  registry-mirrors = [ "https://mirror.gcr.io" ];
+                  #};
+                };
+              };
 
               # ==========================================================
               # PAQUETS SYSTEME (environment.systemPackages)
